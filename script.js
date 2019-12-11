a205c8f668425176c432b5cbdb3f2f8a2f815cb3
hstrodtman <heatherstrodtman@gmail.com>
2019-12-10 21:29:11 -0600

    updated js file for form validation, fetching data and updating CSS properties based on launch status

--- script.js
+++ script.js
@@ -1,13 +1,75 @@
 // Write your JavaScript code here!
 
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
+window.addEventListener('load', function(){
+  
+   fetch('https://handlers.education.launchcode.org/static/planets.json').then(function (response) {
+      return response.json()
+      .then(function (response) {
+         let marsArray = response[3]; 
+         let missionTarget= document.querySelector("#missionTarget"); 
+         missionTarget.innerHTML = `
+         <h2>Mission Destination</h2>
+         <ol>
+            <li>Name: ${marsArray.name}</li>
+            <li>Diameter: ${marsArray.diameter}</li>
+            <li>Star: ${marsArray.star}</li>
+            <li>Distance from Earth: ${marsArray.distance}</li>
+            <li>Number of Moons: ${marsArray.moons}</li>
+         </ol>
+         <img src="${marsArray.image}">
+         `; 
+      })
+  }) 
+
+   let form = document.querySelector('form');  
+   let pilotName = document.querySelector("#pilotName"); 
+   let coPilotName = document.querySelector('input[name=copilotName]'); 
+   let fuelLevel = document.querySelector('input[name=fuelLevel]'); 
+   let cargoMass = document.querySelector('input[name=cargoMass]'); 
+   let button = document.querySelector('#formSubmit'); 
+
+   button.addEventListener("click", function (event){
+      if (pilotName.value === "" || coPilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
+         alert("All fields are required!");
+         event.preventDefault();
+      } 
+      if (isNaN(Number(fuelLevel.value)) || typeof pilotName.value !== 'string' || typeof coPilotName.value !== 'string' || isNaN(Number(cargoMass.value)) ) {
+         alert("Invalid entry"); 
+         event.preventDefault();
+      }
+      let faultyItems = document.querySelector("#faultyItems"); 
+      let fuelStatus = document.querySelector('#fuelStatus'); 
+      let launchStatus = document.querySelector("#launchStatus");   
+      let cargoStatus = document.querySelector("#cargoStatus"); 
+      let pilotStatus = document.querySelector('#pilotStatus'); 
+      let copilotStatus = document.querySelector("#copilotStatus"); 
+      pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready for launch`; 
+      copilotStatus.innerHTML = `Copilot ${coPilotName.value} is ready for launch`; 
+      
+      if(fuelLevel.value < 10000) {
+         faultyItems.style.visibility = "visible"; 
+         fuelStatus.innerHTML = "Fuel level too low for launch."; 
+         launchStatus.innerHTML = "Shuttle not ready for launch"; 
+         launchStatus.style.color = "red"; 
+      }
+      
+      if(cargoMass.value > 10000){
+         faultyItems.style.visibility = "visible";
+         cargoStatus.innerHTML = "Shuttle Mass too high for launch." 
+         launchStatus.innerHTML = "Shuttle not ready for launch"; 
+         launchStatus.style.color = "red"; 
+      }  
+         
+      if(fuelLevel.value > 10000 && cargoMass.value < 10000){
+         faultyItems.style.visibility = "visible";
+         launchStatus.innerHTML = "Shuttle is ready for launch."; 
+         launchStatus.style.color = "green";
+      }
+// I'm not understanding why the page reloads and the CSS values are only briefly displayed. 
+   })
+     
+})
+
+
+
+
