f5ca3b62457ed236726439e18efb9e37357f071b
hstrodtman <heatherstrodtman@gmail.com>
2019-12-11 20:10:58 -0600

    adding else statements and using submit event

--- script.js
+++ script.js
@@ -26,48 +26,44 @@ window.addEventListener('load', function(){
    let coPilotName = document.querySelector('input[name=copilotName]'); 
    let fuelLevel = document.querySelector('input[name=fuelLevel]'); 
    let cargoMass = document.querySelector('input[name=cargoMass]'); 
-   let button = document.querySelector('#formSubmit'); 
+  
 
-   button.addEventListener("click", function (event){
+   form.addEventListener("submit", function (event){
+      event.preventDefault();
+
       if (pilotName.value === "" || coPilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
          alert("All fields are required!");
-         event.preventDefault();
-      } 
-      if (isNaN(Number(fuelLevel.value)) || typeof pilotName.value !== 'string' || typeof coPilotName.value !== 'string' || isNaN(Number(cargoMass.value)) ) {
-         alert("Invalid entry"); 
-         event.preventDefault();
-      }
-      let faultyItems = document.querySelector("#faultyItems"); 
-      let fuelStatus = document.querySelector('#fuelStatus'); 
-      let launchStatus = document.querySelector("#launchStatus");   
-      let cargoStatus = document.querySelector("#cargoStatus"); 
-      let pilotStatus = document.querySelector('#pilotStatus'); 
-      let copilotStatus = document.querySelector("#copilotStatus"); 
-      pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready for launch`; 
-      copilotStatus.innerHTML = `Copilot ${coPilotName.value} is ready for launch`; 
-      
-      if(fuelLevel.value < 10000) {
-         faultyItems.style.visibility = "visible"; 
-         fuelStatus.innerHTML = "Fuel level too low for launch."; 
-         launchStatus.innerHTML = "Shuttle not ready for launch"; 
-         launchStatus.style.color = "red"; 
-      }
-      
-      if(cargoMass.value > 10000){
-         faultyItems.style.visibility = "visible";
-         cargoStatus.innerHTML = "Shuttle Mass too high for launch." 
-         launchStatus.innerHTML = "Shuttle not ready for launch"; 
-         launchStatus.style.color = "red"; 
-      }  
+      } else  if (isNaN(Number(fuelLevel.value)) || typeof pilotName.value !== 'string' || typeof coPilotName.value !== 'string' || isNaN(Number(cargoMass.value)) ) {
+         alert("Invalid entry");
+      } else {
+
+         let faultyItems = document.querySelector("#faultyItems"); 
+         let fuelStatus = document.querySelector('#fuelStatus'); 
+         let launchStatus = document.querySelector("#launchStatus");   
+         let cargoStatus = document.querySelector("#cargoStatus"); 
+         let pilotStatus = document.querySelector('#pilotStatus'); 
+         let copilotStatus = document.querySelector("#copilotStatus"); 
+         pilotStatus.innerHTML = `Pilot ${pilotName.value} is ready for launch`; 
+         copilotStatus.innerHTML = `Copilot ${coPilotName.value} is ready for launch`; 
          
-      if(fuelLevel.value > 10000 && cargoMass.value < 10000){
-         faultyItems.style.visibility = "visible";
-         launchStatus.innerHTML = "Shuttle is ready for launch."; 
-         launchStatus.style.color = "green";
-      }
-// I'm not understanding why the page reloads and the CSS values are only briefly displayed. 
-   })
-     
+         if(fuelLevel.value < 10000) {
+            faultyItems.style.visibility = "visible"; 
+            fuelStatus.innerHTML = "Fuel level too low for launch."; 
+            launchStatus.innerHTML = "Shuttle not ready for launch"; 
+            launchStatus.style.color = "red"; 
+         } else if(cargoMass.value > 10000){
+            faultyItems.style.visibility = "visible";
+            cargoStatus.innerHTML = "Shuttle Mass too high for launch." 
+            launchStatus.innerHTML = "Shuttle not ready for launch"; 
+            launchStatus.style.color = "red"; 
+         }  else if (fuelLevel.value > 10000 && cargoMass.value < 10000){
+            faultyItems.style.visibility = "visible";
+            launchStatus.innerHTML = "Shuttle is ready for launch."; 
+            launchStatus.style.color = "green";
+         }
+
+      }   
+   })   
 })
 
 
